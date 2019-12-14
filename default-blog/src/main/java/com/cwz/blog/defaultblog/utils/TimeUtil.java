8add3299cc4870d010a46ff0cb53c858b9734995
WenZ <948009390@qq.com>
2019-12-14 21:06:12 +0800

    旧版本提交

---
+++ default-blog/src/main/java/com/cwz/blog/defaultblog/utils/TimeUtil.java
@@ -0,0 +1,120 @@
+package com.cwz.blog.defaultblog.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+/**
+ * @author: 陈文振
+ * @date: 2019/12/4
+ * @description: 时间工具
+ */
+public class TimeUtil {
+
+    /**
+     * @description: 格式化日期
+     * 使用线程安全的DateTimeFormatter
+     * @author: 陈文振
+     * @date: 2019/12/4
+     * @return: “年-月-日”字符串
+     */
+    public String getFormatDateForThree(){
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return now.format(formatter);
+    }
+
+    /**
+     * @description: 格式化日期
+     * @author: 陈文振
+     * @date: 2019/12/4
+     * @return: “年-月-日 时:分:秒”字符串
+     */
+    public String getFormatDateForSix(){
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return now.format(formatter);
+    }
+
+    /**
+     * @description: 格式化日期
+     * @author: 陈文振
+     * @date: 2019/12/4
+     * @return: “年-月-日 时:分”字符串
+     */
+    public String getFormatDateForFive(){
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return now.format(formatter);
+    }
+
+    /**
+     * @description: 解析日期
+     * @author: 陈文振
+     * @date: 2019/12/4
+     * @param date: 日期 2019-12-4
+     * @return:
+     */
+    public LocalDate getParseDateForThree(String date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return LocalDate.parse(date, formatter);
+    }
+
+    /**
+     * @description: 解析日期
+     * @author: 陈文振
+     * @date: 2019/12/4
+     * @param date: 日期 2019-12-4 16:43:20
+     * @return:
+     */
+    public LocalDate getParseDateForSix(String date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return LocalDate.parse(date, formatter);
+    }
+
+    /**
+     * @description: 获得当前时间的时间戳
+     * @author: 陈文振
+     * @date: 2019/12/4
+     * @return: 时间戳
+     */
+    public long getLongTime(){
+        Date now = new Date();
+        return now.getTime()/1000;
+    }
+
+    /**
+     * @description: 时间中横杆转换为年
+     * 线程安全使用 StringBuffer
+     * @author: 陈文振
+     * @date: 2019/12/4
+     * @param str: 2019-12
+     * @return: 2019年12月
+     */
+    public String timeWhippletreeToYear(String str){
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(str.substring(0, 4));
+        stringBuffer.append("年");
+        stringBuffer.append(str.substring(5,7));
+        stringBuffer.append("月");
+        return String.valueOf(stringBuffer);
+    }
+
+    /**
+     * @description: 时间中的年转换为横杠
+     * 线程安全使用 StringBuffer
+     * @author: 陈文振
+     * @date: 2019/12/4
+     * @param str: 2019年12月
+     * @return: 2019-12
+     */
+    public String timeYearToWhippletree(String str){
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(str.substring(0, 4));
+        stringBuffer.append("-");
+        stringBuffer.append(str.substring(5,7));
+        return String.valueOf(stringBuffer);
+    }
+
+}
